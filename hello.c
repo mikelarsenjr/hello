d4f0a0be1a65e14e9782c8f5f21c2dc6aeb73bda
Mike <mlarsen11@huskers.unl.edu>
2022-01-28 16:19:50 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Mike Larsen
+ * Date: 2022/01/28
+ * 
+ *Program that prints simple message.
+ */
+ #include <stdlib.h>
+ #include <stdio.h>
+ 
+ int main(int argc, char **argv) {
+     
+     printf("Mike Larsen\n");
+     
+     
+     return 0;
+     
+ }
